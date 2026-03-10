c9fbdab7525f0905505b6d5a8fb059e1147bff1b
pavanraj077 <pavanraj7403@gmail.com>
2026-03-10 12:01:21 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function sendMail()
+{
+	window.location.href="mailto:pavanraj7403@gmail.com";
+}
+
+function openGitHub()
+{
+	window.open("https://github.com/pavanraj077","_blank");
+}
+
+function openLinkedIn()
+{
+	window.open("https://linkedin.com/in/pavan-raj-madira-b4b06a345","_blank");
+}
+
+function openProject()
+{
+	window.open("https://github.com/pavanraj077/HospitalmanagementSystem","_blank");
+}
